56f733d9338fb5fe37e2803f580dbcad173dbcb9
marcin388 <p14170863@my365.dmu.ac.uk>
2019-07-09 19:50:38 +0100

    Added Event Handling and Window Abstraction.

---
+++ Hazel/include/Window.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "hzpch.h"
+
+#include "Core.h"
+#include "Events\Event.h"
+
+namespace Hazel {
+	struct WindowProps
+	{
+		std::string Title;
+		unsigned int Width;
+		unsigned int Height;
+
+		WindowProps(const std::string& title = "Hazel Engine",
+			unsigned int width = 1280, unsigned int height = 720)
+			: Title(title), Width(width), Height(height) {}
+	};
+
+	// Interface representing a dekstop system based Window
+	class HAZEL_API Window
+	{
+	public:
+		using EventCallBackFn = std::function<void(Event&)>;
+
+		virtual ~Window() {}
+
+		virtual void OnUpdate() = 0;
+
+		virtual unsigned int GetWidth() const = 0;
+		virtual unsigned int GetHeight() const = 0;
+
+		//Window attributes
+		virtual void SetEventCallback(const EventCallBackFn& callback) = 0;
+		virtual void SetVSync(bool enabled) = 0;
+		virtual bool IsVSync() const = 0;
+
+		static Window* Create(const WindowProps& props = WindowProps());
+	};
+
+}
--- Hazel/src/Application.cpp
+++ Hazel/src/Application.cpp
@@ -1,9 +1,15 @@
+#include "hzpch.h"
 #include "Application.h"
+
+#include "Events\MouseEvent.h"
+
+#include <GLFW\glfw3.h>
 
 namespace Hazel {
 
 	Application::Application()
 	{
+		m_Window = std::unique_ptr<Window>(Window::Create());
 	}
 
 
@@ -13,7 +19,12 @@ namespace Hazel {
 
 	void Application::Run()
 	{
-		while (true);
+		while (m_Running)
+		{
+			glClearColor(1, 0, 1, 1);
+			glClear(GL_COLOR_BUFFER_BIT);
+			m_Window->OnUpdate();
+		}
 	}
 
 }
